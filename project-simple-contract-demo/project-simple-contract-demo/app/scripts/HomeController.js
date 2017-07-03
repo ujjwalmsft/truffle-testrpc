0e7c3435da88c06473bf7c1fbc3ebd23f725bc61
dr-dolittle <dolittle@posteo.net>
2017-07-03 11:56:09 +0200

    delete project

--- project-simple-contract-demo/project-simple-contract-demo/app/scripts/HomeController.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var app = angular.module("contract-demo");
-    
-    var HomeController = function($scope){
-        $scope.header = "Hello!";
-    };
-
-    app.controller("HomeController", HomeController);
-}());
